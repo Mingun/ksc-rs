650e0a2f875079b86d1817f23f0e4501e0b205de
Mingun <alexander_sergey@mail.ru>
2021-11-11 23:31:26 +0500

    java: Add skeleton of Java generator and test helper function for compile Java code using system javac

---
+++ ksc-java/src/lib.rs
@@ -0,0 +1,46 @@
+//! Compiler backend for generate a Java source code from the Kaitai Struct definition.
+
+/// Translates Kaitai Struct definition to Java 8 source code
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash)]
+pub struct JavaGenerator;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/// Compile specified `source` as a java code and check that it has no errors.
+/// The source should contain definition of the `KscJavaTest` class.
+///
+/// # Parameters
+/// - `dir`: directory in which source file should be placed
+/// - `source`: Java source code to compile
+#[cfg(test)]
+#[track_caller]
+fn compile(dir: &std::path::Path, source: &str) {
+  use std::fs::File;
+  use std::io::Write;
+  use std::process::Command;
+
+  let path = std::env::temp_dir().join(dir);
+  std::fs::create_dir_all(&path).expect("cannot create directory for test");
+
+  let path = path.join("KscJavaTest.java");
+  let mut java = File::create(&path).expect("cannot create temp file with java code");
+  java.write_all(source.as_ref()).expect("cannot write Java source code to the file");
+
+  let output = Command::new("javac")
+    .arg(path.as_os_str())
+    .output()
+    .expect("failed to execute javac. Check that `javac` in the PATH. Is JDK installed?");
+
+  println!(r#"
+status: {}
+stdout(empty={}):
+{}
+stderr(empty={}):
+{}
+"#,
+    output.status,
+    output.stdout.is_empty(), String::from_utf8_lossy(&output.stdout),
+    output.stderr.is_empty(), String::from_utf8_lossy(&output.stderr),
+  );
+  assert!(output.status.success());
+}
