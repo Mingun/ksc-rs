d0db418630e6a930e9fc7e9a4b66a5d8d1d52003
Mingun <alexander_sergey@mail.ru>
2021-12-02 18:59:50 +0500

    Add `kaitai_struct_tests` as a submodule
    failures (1):
        formats::parse_test_data_formats_type_ternary_2nd_falsy_ksy

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub mod parser;
 
 /// Expected, that https://github.com/kaitai-io/kaitai_struct_formats was checkout to
 /// crate root directory, next to `src`.
+/// Expected, that https://github.com/kaitai-io/kaitai_struct_tests was checkout to
+/// the `test-data` in the crate root directory, next to `src`.
 #[cfg(test)]
 mod formats {
   use crate::model::Root;
@@ -17,9 +19,23 @@ mod formats {
   use test_generator::test_resources;
 
   #[test_resources("formats/**/*.ksy")]
+  #[test_resources("test-data/formats/**/*.ksy")]
   fn parse(resource: &str) {
     let file = File::open(resource).expect(&format!("can't read file {}", resource));
     let ksy: Ksy = serde_yml::from_reader(file).expect(&format!("invalid file {}", resource));
     let _: Root = ksy.try_into().expect(&format!("incorrect KSY {}", resource));
   }
+  /*TODO: включить ошибочные тесты, когда они прекратят падать (== будет сделан полный разбор)
+  #[test_resources("test-data/formats_err/**/*.ksy")]
+  fn error(resource: &str) {
+    let file = File::open(resource).expect(&format!("can't read file {}", resource));
+    let ksy: Result<Ksy, _> = serde_yml::from_reader(file);
+    match ksy {
+      Ok(ksy) => {
+        let root: Result<Root, _> = ksy.try_into();
+        root.expect_err(&format!("correct KSY {}", resource));
+      },
+      _ => (),
+    }
+  }*/
 }
